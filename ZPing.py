ed14fbbd2a01fc59952d3f47e939f02cb593654a
ylws-4617 <yuluowusheng4617@gmail.com>
2018-03-09 16:27:09 -0500

    使用系统Ping测试延迟

--- ZPing.py
+++ ZPing.py
@@ -14,6 +14,16 @@ import struct
 import select
 import json
 import time
+import commands
+
+def ping(host):
+    cmd = "ping "+ str(host) + " -c1 -W 1"
+    result = commands.getoutput(cmd)
+    result = result.split()
+    result = result[-2].split("/")[0]
+    if result.isalpha():
+	result = False
+    return float(result)
 
 STYLE = {
     'fore': {
@@ -76,144 +86,12 @@ D = {
     }
 
 
-ICMP_ECHO_REQUEST = 8
-DEFAULT_TIMEOUT = 2
-DEFAULT_COUNT = 3
-
-class Pinger(object):
-	""" Pings to a host -- the Pythonic way"""
-	
-	def __init__(self, target_host, count=DEFAULT_COUNT, timeout=DEFAULT_TIMEOUT):
-		self.target_host = target_host
-		self.count = count
-		self.timeout = timeout
-		self.delay_list=list()
-		
-
-
-	def do_checksum(self, source_string):
-		"""  Verify the packet integritity """
-		sum = 0
-		max_count = (len(source_string)/2)*2
-		count = 0
-		while count < max_count:
-			val = ord(source_string[count + 1])*256 + ord(source_string[count])
-			sum = sum + val
-			sum = sum & 0xffffffff 
-			count = count + 2
-	 
-		if max_count<len(source_string):
-			sum = sum + ord(source_string[len(source_string) - 1])
-			sum = sum & 0xffffffff 
-	 
-		sum = (sum >> 16)  +  (sum & 0xffff)
-		sum = sum + (sum >> 16)
-		answer = ~sum
-		answer = answer & 0xffff
-		answer = answer >> 8 | (answer << 8 & 0xff00)
-		return answer
- 
-	def receive_pong(self, sock, ID, timeout):
-		"""
-		Receive ping from the socket.
-		"""
-		time_remaining = timeout
-		while True:
-			start_time = time.time()
-			readable = select.select([sock], [], [], time_remaining)
-			time_spent = (time.time() - start_time)
-			if readable[0] == []: # Timeout
-				return
-	 
-			time_received = time.time()
-			recv_packet, addr = sock.recvfrom(1024)
-			icmp_header = recv_packet[20:28]
-			type, code, checksum, packet_ID, sequence = struct.unpack(
-				"bbHHh", icmp_header
-			)
-			if packet_ID == ID:
-				bytes_In_double = struct.calcsize("d")
-				time_sent = struct.unpack("d", recv_packet[28:28 + bytes_In_double])[0]
-				return time_received - time_sent
-	 
-			time_remaining = time_remaining - time_spent
-			if time_remaining <= 0:
-				return
-	 
-	 
-	def send_ping(self, sock,  ID):
-		"""
-		Send ping to the target host
-		"""
-		target_addr  =  socket.gethostbyname(self.target_host)
-	 
-		my_checksum = 0
-	 
-		# Create a dummy heder with a 0 checksum.
-		header = struct.pack("bbHHh", ICMP_ECHO_REQUEST, 0, my_checksum, ID, 1)
-		bytes_In_double = struct.calcsize("d")
-		data = (192 - bytes_In_double) * "Q"
-		data = struct.pack("d", time.time()) + data
-	 
-		# Get the checksum on the data and the dummy header.
-		my_checksum = self.do_checksum(header + data)
-		header = struct.pack(
-			"bbHHh", ICMP_ECHO_REQUEST, 0, socket.htons(my_checksum), ID, 1
-		)
-		packet = header + data
-		sock.sendto(packet, (target_addr, 1))
-	 
-	 
-	def ping_once(self):
-		"""
-		Returns the delay (in seconds) or none on timeout.
-		"""
-		icmp = socket.getprotobyname("icmp")
-		try:
-			sock = socket.socket(socket.AF_INET, socket.SOCK_RAW, icmp)
-		except socket.error, (errno, msg):
-			if errno == 1:
-				# Not superuser, so operation not permitted
-				msg +=  "ICMP messages can only be sent from root user processes"
-				raise socket.error(msg)
-		except Exception, e:
-			print "Exception: %s" %(e)
-	
-		my_ID = os.getpid() & 0xFFFF
-	 
-		self.send_ping(sock, my_ID)
-		delay = self.receive_pong(sock, my_ID, self.timeout)
-		sock.close()
-		return delay
-	 
-	 
-	def ping(self):
-		"""
-		Run the ping process
-		"""
-		for i in xrange(self.count):
-			try:
-				delay  =  self.ping_once()
-			except socket.gaierror, e:
-				return False
-				break
-	 
-			if delay  ==  None:
-				return False
-			else:
-				delay  =  delay * 1000
-				self.delay_list.append(delay)
-		return (sum(self.delay_list)/len(self.delay_list))
-
-
-count = 1
 string =list()
 d=dict()
 
 for x in D:
     host=D[x]
-    pinger = Pinger(host)
-    result = pinger.ping()
+    result = ping(host)
 	
 	
     if result == False:
